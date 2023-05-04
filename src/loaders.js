1282835d04a9ddd7d6fefc311f477d621e378c71
Joe Keohan <jkeohan@gmail.com>
2023-05-04 18:28:32 -0400

    Update loaders.js

--- src/loaders.js
+++ src/loaders.js
@@ -5,7 +5,7 @@ export const priceLoader = async ({ params }) => {
 
 	return (
 		await fetch(
-			`http://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`
+			`https://rest-sandbox.coinapi.io/v1/exchangerate/${symbol}/USD?apikey=${apiKey}`
 		)
 	).json();
 };
